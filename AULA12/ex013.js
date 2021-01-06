d6d53c64488e6fc108d45711b76459d2acd1b528
Diógenes Souza <diogenes.edusouza@gmail.com>
2021-01-06 00:21:42 -0300

    Update curso JS
    Aulas 12 e 13

--- AULA12/ex013.js
+++ AULA12/ex013.js
@@ -5,8 +5,6 @@ if (hora < 12){
     console.log('Bom dia!')
 } else if (hora <= 18){
     console.log('Boa tarde!')
-} else if (hora > 23 && hora > 6){
-    console.log('Boa mdrugada!')
 } else{
     console.log('Boa noite!')
 } 
