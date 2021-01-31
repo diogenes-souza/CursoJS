fa202c4d785495abbcb645849f5783ec6c675eed
Diógenes Souza <diogenes.edusouza@gmail.com>
2021-01-31 20:08:17 -0300

    Update Ex Aula 14
    Update Ex Aula 14

---
+++ Ex_JS_Parte4/script.js
@@ -0,0 +1,8 @@
+function contar() {
+    let nInicio = document.getElementById('inicio')
+    let nFim = document.getElementById('fim')
+    let nPasso = document.getElementById('passo')
+    let resultado = document.getElementById('res')
+
+    
+}
