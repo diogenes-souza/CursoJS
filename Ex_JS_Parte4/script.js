56f8e156d7f6ea2c8c3938deb93af11348474921
Diógenes Souza <diogenes.edusouza@gmail.com>
2021-02-01 01:16:38 -0300

    Uodate Ex. Parte 4

--- Ex_JS_Parte4/script.js
+++ Ex_JS_Parte4/script.js
@@ -4,5 +4,21 @@ function contar() {
     let nPasso = document.getElementById('passo')
     let resultado = document.getElementById('res')
 
+    if(nInicio.value.length == 0 && nFim.value.length == 0 && nPasso.value.length == 0) {
+        resultado.innerHTML = 'Preencha os campos!!!'
+    }else if (nInicio.value.length == 0 || nFim.value.length == 0 || nPasso.value.length == 0) {
+        resultado.innerHTML = 'Impossível contar.'
+    }else if (nPasso.value == 0){
+        nPasso = 1
+        resultado.innerHTML = 'Contando...'
+        for(nInicio; nInicio <= nFim; nPasso){
+            resultado.innerHTML = `${nInicio} -> `
+        }
+    }else{
+        resultado.innerHTML = 'Contando...'
+        // for(nInicio.value; nInicio.value <= nFim.value; nPasso.value){
+        //     resultado.innerHTML = `${nInicio} -> `
+        // }
+    }
     
 }
